6a84c188584c39055bd4a31a33879a655158c840
Ahmed Ayman <ahmed.a.elkemary@gmail.com>
2019-09-29 22:49:41 +0200

    add production files

--- server.js
+++ server.js
@@ -2,6 +2,6 @@ const express = require('express');
 const app = express();  
 app.use(express.static(__dirname + '/dist'));  
 app.get('*', (req, res) => {  
-  res.status(200).sendFile('/dist/index.html');  
+  res.status(200).sendFile(__dirname + '/dist/index.html');  
 });  
 app.listen(process.env.PORT || 8080);  
