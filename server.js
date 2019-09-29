46997ef9feea4dad7c2d7387dfdc406d1c3da4ea
Ahmed Ayman <ahmed.a.elkemary@gmail.com>
2019-09-29 22:39:23 +0200

    setup heroku

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');  
 const app = express();  
 app.use(express.static(__dirname + '/dist'));  
-app.all('*', (req, res) => {  
-  res.status(200).sendFile(__dirname + '/dist/index.html');  
+app.get('*', (req, res) => {  
+  res.status(200).sendFile('/dist/index.html');  
 });  
 app.listen(process.env.PORT || 8080);  
